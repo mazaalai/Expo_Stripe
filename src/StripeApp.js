3f84942dc72aa8993db2bb067df5b05d1d3bd102
mazaalai <r0745664@student.thomasmore.be>
2022-03-21 12:23:50 +0100

    1. Cardfield from Stripe App.

--- src/StripeApp.js
+++ src/StripeApp.js
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
 import {View, TextInput, StyleSheet,Text} from "react-native";
+import {CardField} from "@stripe/stripe-react-native";
 
 const StripeApp = (props) => {
     const[email, setEmail] = useState();
+    const [cardDetails, setCardDetails] = useState();
 return (
     <View style={styles.container}>
+
         <TextInput
             autoCapitalize="none"
             placeholder="E-mail"
@@ -12,9 +15,17 @@ return (
             onChange={value => setEmail(value.nativeEvent.text)}
             style={styles.input}
         />
-    <Text>
-        Stripe App
-    </Text>
+        <CardField
+            postalCodeEnabled={true}
+            placeholder={{
+                number: "4242 4242 4242 4242",
+            }}
+            cardStyle={styles.card}
+            style={styles.cardContainer}
+            onCardChange={cardDetails => {
+                setCardDetails(cardDetails);
+            }}
+        />
     </View>
 );
 }
@@ -22,8 +33,8 @@ export default StripeApp;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
+        margin: 20,
     },
     input: {
         backgroundColor: "#efefefef",
@@ -32,5 +43,12 @@ const styles = StyleSheet.create({
         fontSize: 20,
         height: 50,
         padding: 10,
+    },
+    card: {
+        backgroundColor: "#efefefef",
+    },
+    cardContainer: {
+        height: 50,
+        marginVertical: 30,
     },
 });
